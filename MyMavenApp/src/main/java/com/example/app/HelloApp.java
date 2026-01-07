c7757fb3ff3bef0fdf1e031f80a4892d4e09109a
G.Nishanth <2420030679@klh.edu.in>
2026-01-07 11:51:56 +0530

    Add files via upload

---
+++ MyMavenApp/src/main/java/com/example/app/HelloApp.java
@@ -0,0 +1,10 @@
+package com.example.app;
+
+public class HelloApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("HI This is my new maven project");
+	}
+
+}
